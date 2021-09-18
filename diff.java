de089b3f23e782a3d1cc4318cb0d35c39f435b21
Abhinay Sinha <abhinay.sinha_cs.aiml20@gla.ac.in>
2021-09-18 15:17:40 +0530

    difference

--- diff.java
+++ diff.java
@@ -1,2 +1,13 @@
-package Questions;public class diff {
+package Questions;
+
+import java.util.Scanner;
+
+public class diff {
+         public static void main(String[] args) {
+             Scanner sc= new Scanner(System.in);
+             int a,b;
+             a=sc.nextInt();
+             b=sc.nextInt();
+             System.out.println(a-b);
+         }
 }
